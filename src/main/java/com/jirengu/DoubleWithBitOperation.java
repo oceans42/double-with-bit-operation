3bd8fe9db2ed3288ac192effcd3cd2ff90f61bee
hyq <958575100@qq.com>
2024-05-04 19:57:59 +0800

    Update DoubleWithBitOperation.java

--- src/main/java/com/jirengu/DoubleWithBitOperation.java
+++ src/main/java/com/jirengu/DoubleWithBitOperation.java
@@ -3,7 +3,7 @@ package com.jirengu;
 public class DoubleWithBitOperation {
     public static int doubleWithBitOperation(int num) {
         // 使用位运算符将输入的整数乘以2
-        return num <<;
+        return num << 1;
     }
 
 }
